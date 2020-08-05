f51d949f78686ea0ef94087f27bcfa5c57b774b9
mukesh630 <67049346+mukesh630@users.noreply.github.com>
2020-08-05 11:39:08 +0530

    add

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div className="container">
+        <div classNmae="py-4">
+        <h1> About Page</h1>
+        </div>
+        </div>
+    );
+};
+
+
+export default About;
